56b640bf10929245d9cef3f667029afbcee61588
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-12-16 17:34:47 +0530

    16 Dec

---
+++ src/ArrayList/Iterate_Arraylist.java
@@ -0,0 +1,21 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Iterate_Arraylist {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        List<String > list_string=new ArrayList<>();
+        System.out.println("Enter number of the member");
+        int n=sc.nextInt();
+        System.out.println("Enter the names of the member");
+        for(int i=0;i<n;i++){
+            list_string.add(sc.next());
+        }
+        for(String item:list_string){
+            System.out.println(item);
+        }
+    }
+}
