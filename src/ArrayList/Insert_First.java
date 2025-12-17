353aafce5a7034cb6c87bc13e3296b0a694ebc3a
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-12-17 11:42:40 +0530

    17 Dec

---
+++ src/ArrayList/Insert_First.java
@@ -0,0 +1,27 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Insert_First {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        List<String>stringList=new ArrayList<String>();
+        System.out.println("Enter total length of the list");
+        int n=sc.nextInt();
+        System.out.println("Enter the string element :");
+        for(int i=0;i<n;i++){
+            stringList.add(sc.next());
+        }
+        for(String lists:stringList){
+            System.out.println(lists);
+        }
+        System.out.println("Enter the string which we have to add at first position: ");
+        stringList.add(0,sc.next());
+
+        for(String lists:stringList){
+            System.out.println(lists);
+        }
+    }
+}
