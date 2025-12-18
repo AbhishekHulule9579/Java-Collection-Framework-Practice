c3d1b8db140dd30334704448c6481a4c733a2a2d
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-12-18 10:03:37 +0530

    18 Dec

---
+++ src/ArrayList/Retrieve_Element.java
@@ -0,0 +1,26 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Retrieve_Element {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        List<String> stringList=new ArrayList<String>();
+        System.out.println("Enter the list number");
+        int n=sc.nextInt();
+        System.out.println("Enter the list element");
+        for(int i=0;i<n;i++){
+            stringList.add(sc.next());
+        }
+        for(String ls:stringList){
+            System.out.println(ls);
+        }
+        String element=stringList.get(0);
+        System.out.println(element);
+        System.out.println(stringList.get(3));
+
+    }
+
+}
