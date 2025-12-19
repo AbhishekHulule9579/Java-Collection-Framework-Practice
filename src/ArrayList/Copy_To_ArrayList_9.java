01df5a0ce0ac27c10fb34f41a9451a6477bb88c3
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-12-19 18:23:16 +0530

    19 Dec

---
+++ src/ArrayList/Copy_To_ArrayList_9.java
@@ -0,0 +1,34 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Copy_To_ArrayList_9 {
+    public static void main(String[] args) {
+        List<String> List1=new ArrayList<String>();
+        List1.add("Red");
+        List1.add("Green");
+        List1.add("Orange");
+        List1.add("White");
+        List1.add("Black");
+        for(String str:List1){
+            System.out.print(str+" ");
+        }
+        System.out.println();
+        List<String> List2=new ArrayList<String>();
+        List2.add("1");
+        List2.add("2");
+        List2.add("3");
+        List2.add("4");
+        List2.add("5");
+        for(String str:List2){
+            System.out.print(str+" ");
+        }
+        System.out.println();
+        System.out.println("Copy List from list1 to list 2 after copy");
+        Collections.copy(List1,List2);
+        System.out.println("List 1 "+ List1);
+        System.out.println("List 2 "+ List2);
+    }
+}
