cb5da22b95514b5ee7036a315b21cb2217e73d31
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-12-24 13:51:58 +0530

    24 Dec

---
+++ src/LinkedList/L1_Append_Element.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class L1_Append_Element {
+    public static void main(String[] args) {
+        LinkedList<String>l_list=new LinkedList<String>();
+        l_list.add("Black");
+        l_list.add("Blue");
+        l_list.add("Red");
+        l_list.add("Yellow");
+        l_list.add("Grey");
+
+        System.out.println("The linkedlist: "+l_list);
+    }
+}
