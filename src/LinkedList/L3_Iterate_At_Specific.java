59d757cc4b4b996f261cba916861ae11bbaff5ca
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-12-25 22:33:35 +0530

    25 Dec

--- src/LinkedList/L3_Iterate_At_Specific.java
+++ src/LinkedList/L3_Iterate_At_Specific.java
@@ -5,6 +5,6 @@ import java.util.LinkedList;
 public class L3_Iterate_At_Specific {
     public static void main(String[] args) {
         LinkedList<String>l_list=new LinkedList<String>();
-        
+        l_list.add("Black");
     }
 }
