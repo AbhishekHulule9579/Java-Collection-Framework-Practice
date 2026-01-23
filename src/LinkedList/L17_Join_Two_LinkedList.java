2ded3117b7586f87fc305bc84e701cbf3cae22fb
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-01-23 08:36:47 +0530

    23 Jan

--- src/LinkedList/L17_Join_Two_LinkedList.java
+++ src/LinkedList/L17_Join_Two_LinkedList.java
@@ -1,5 +1,23 @@
 package LinkedList;
 
-public class L17_Join_Two_LinkedList {
+import java.util.LinkedList;
 
+public class L17_Join_Two_LinkedList {
+    public static void main(String[] args) {
+        LinkedList<String>c1=new LinkedList<>();
+        c1.add("Red");
+        c1.add("Green");
+        c1.add("Black");
+        c1.add("White");
+        c1.add("Pink");
+        LinkedList<String>c2=new LinkedList<>();
+        c2.add("Blue");
+        c2.add("Yellow");
+        c2.add("Violet");
+        c2.add("White");
+        LinkedList<String>a=new LinkedList<>();
+        a.addAll(c1);
+        a.addAll(c2);
+        System.out.println("the linked list after adding the linkedlist to each other "+a);
+    }
 }
---
+++ src/LinkedList/L18_Copy_LinkedList.java
@@ -0,0 +1,9 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class L18_Copy_LinkedList {
+    public static void main(String[] args) {
+        LinkedList<String>c1=new LinkedList<>();
+    }
+}
