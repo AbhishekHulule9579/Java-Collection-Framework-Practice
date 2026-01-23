36db2f32ed4763a8adf610dd076edce494a01d7e
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-01-23 08:41:25 +0530

    23 Jan

--- src/LinkedList/L18_Copy_LinkedList.java
+++ src/LinkedList/L18_Copy_LinkedList.java
@@ -5,5 +5,13 @@ import java.util.LinkedList;
 public class L18_Copy_LinkedList {
     public static void main(String[] args) {
         LinkedList<String>c1=new LinkedList<>();
+        c1.add("Red");
+        c1.add("Green");
+        c1.add("Black");
+        c1.add("White");
+        c1.add("Pink");
+        LinkedList<String>c2=new LinkedList<>();
+        c2=(LinkedList)c1.clone();
+        System.out.println("The linked list after the cloning "+c2);
     }
 }
