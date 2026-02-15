fee71c7d5c88e3b9fbb7be20c593f8554d8d2b0a
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-02-15 22:31:12 +0530

    15 feb

--- src/List/RemoveDuplicate.java
+++ src/List/RemoveDuplicate.java
@@ -1,6 +1,14 @@
 package List;
 
+import java.util.HashMap;
+
 public class RemoveDuplicate {
+    public static void removedupli(String str){
+        HashMap<Character,Integer>map=new HashMap<>();
+
+    }
     public static void main(String[] args) {
+        String str="Hello World";
+        removedupli(str);
     }
 }
