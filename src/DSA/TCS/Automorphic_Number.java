32d048accfe7823182b6897f259c9b710738ed31
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-03-07 23:24:41 +0530

    7 march

--- src/DSA/TCS/Automorphic_Number.java
+++ src/DSA/TCS/Automorphic_Number.java
@@ -22,6 +22,7 @@ public class Automorphic_Number {
             System.out.println("Not Automorphic Number");
     }
 }
+
 /*
  A number is called an Automorphic number if and only if
  * its square ends in the same digits as the number itself.
