66a8e37b6b4f1b9c31a79531ac3c5ae29e8d66e9
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-03-25 19:25:19 +0530

    25 march

---
+++ src/TreeSet/T14_Poll_First_TreeSet.java
@@ -0,0 +1,23 @@
+package TreeSet;
+
+import java.util.TreeSet;
+
+public class T14_Poll_First_TreeSet {
+    public static void main(String[] args) {
+        TreeSet<Integer> tree_num=new TreeSet<Integer>();
+
+        tree_num.add(10);
+        tree_num.add(22);
+        tree_num.add(36);
+        tree_num.add(25);
+        tree_num.add(16);
+        tree_num.add(76);
+        tree_num.add(82);
+        tree_num.add(89);
+        tree_num.add(14);
+
+        System.out.println("Original TreeSet: "+tree_num);
+        System.out.println("Remove the first (lowest) element: "+tree_num.pollFirst());
+        System.out.println("treeset after removing the poll first (lowest) element "+tree_num);
+    }
+}
