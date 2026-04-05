e3d892a90a67ad4ec6378adbe31fa936d1b035f2
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-04-05 20:38:20 +0530

    05 april

---
+++ src/DSA/TCS_Interview/T21_Currency_Conversion.java
@@ -0,0 +1,12 @@
+package DSA.TCS_Interview;
+
+public class T21_Currency_Conversion {
+    public static void main(String[] args) {
+        double rates=94.45;
+
+        double available_usd=150;
+
+        double inr=rates*available_usd;
+        System.out.println(inr);
+    }
+}
