a91e1e5fff25dbdcc829c3aee4028e79cc2e55e0
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-04-07 17:50:02 +0530

    07 april

---
+++ src/DSA/TCS_Interview/T22_Inverted_Numbered_Pyramid.java
@@ -0,0 +1,16 @@
+package DSA.TCS_Interview;
+
+public class T22_Inverted_Numbered_Pyramid {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=n;i>=1;i--){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(k+" ");
+            }
+            System.out.println();
+        }
+    }
+}
