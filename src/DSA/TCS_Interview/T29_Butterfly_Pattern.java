f77f3619115544277736ffd149a2e3b209c934e7
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-04-17 07:52:50 +0530

    17 april

---
+++ src/DSA/TCS_Interview/T29_Butterfly_Pattern.java
@@ -0,0 +1,51 @@
+package DSA.TCS_Interview;
+
+public class T29_Butterfly_Pattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+
+            int spaces = 2 * (n - i);
+            for(int j = 1; j <= spaces; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+
+            int spaces = 2 * (n - i);
+            for(int j = 1; j <= spaces; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+
+ *      *
+ **    **
+ ***  ***
+ ********
+ ********
+ ***  ***
+ **    **
+ *      *
+
+ */
