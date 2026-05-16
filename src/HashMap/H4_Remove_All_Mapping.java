3132ec10ad366d7eb005f42410a2cef80091582f
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-05-16 08:20:23 +0530

    16 May

---
+++ src/HashMap/H4_Remove_All_Mapping.java
@@ -0,0 +1,17 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class H4_Remove_All_Mapping {
+    public static void main(String[] args) {
+        HashMap<Integer,String> map1=new HashMap<Integer, String>();
+        map1.put(1,"Black");
+        map1.put(2,"Blue");
+        map1.put(3,"Yellow");
+        map1.put(4,"Red");
+        map1.put(5,"Violet");
+        System.out.println("All values are "+map1);
+        map1.clear();
+        System.out.println("Values are "+map1);
+    }
+}
