7fe62504b3368b9d2a379f58548f14e713fcae27
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-05-28 17:38:05 +0530

    28 May

---
+++ src/TreeMap/T5_Get_All_Keys.java
@@ -0,0 +1,19 @@
+package TreeMap;
+
+import java.util.Set;
+import java.util.TreeMap;
+
+public class T5_Get_All_Keys {
+    public static void main(String[] args) {
+        TreeMap<String,String>treeMap=new TreeMap<String, String>();
+        treeMap.put("t1","Red");
+        treeMap.put("t2","Black");
+        treeMap.put("t3","Blue");
+        treeMap.put("t4","Yellow");
+
+        Set<String> key=treeMap.keySet();
+        for(String keys:key){
+            System.out.println(keys);
+        }
+    }
+}
