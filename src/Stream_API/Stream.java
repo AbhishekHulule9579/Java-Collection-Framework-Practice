3fe668a5e8060b73a09fb57e1782a58dbe9715f7
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-09-12 13:02:22 +0530

    12 sep

---
+++ src/Stream_API/Stream.java
@@ -0,0 +1,22 @@
+package Stream_API;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream {
+    public static void main(String[] args) {
+        ArrayList<Integer>l=new ArrayList<Integer>();
+        l.add(0);
+        l.add(10);
+        l.add(20);
+        l.add(9);
+        l.add(40);
+        l.add(43);
+        l.add(50);
+
+        System.out.println(l);
+        List<Integer>l1=l.stream().filter(i->i%2==0).collect(Collectors.toList());
+        System.out.println(l1);
+    }
+}
